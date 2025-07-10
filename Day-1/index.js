0790bddec8103950148514bc79b844dc14105bc8
tejasmhatre8828 <tejasmhatre0234@gmail.com>
2025-07-10 12:28:41 +0530

    day 1

---
+++ Day-1/index.js
@@ -0,0 +1,71 @@
+
+let age = 20;
+
+
+
+
+let tex="Hello World!"
+console.log(tex)
+let str="welcome to javascript"
+console.log(str, typeof "str")
+
+
+
+
+var num=1233445
+console.log(num, typeof num)
+
+
+
+
+
+const bool=false
+console.log(bool, typeof bool)
+// const bool=true
+// console.log(bool, typeof bool)
+
+
+
+
+var undefinedValue
+console.log(undefinedValue, typeof undefinedValue)
+
+
+let Value=null
+console.log(Value, typeof Value)
+
+
+var counter=5
+counter=counter + 1
+counter++
+counter += 1
+console.log("Increment Operator", counter)
+
+var counter=5
+counter=counter - 1
+counter--
+counter -= 1
+console.log("Decrement Operator", counter)
+
+var name1="Tejas "
+var name2= `Mhatre`
+console.log(name1 + name2)
+
+
+
+
+var num=10
+var num2=5
+console.log("Addition Operator", num + num2 + num)
+console.log("Subtraction Operator", num2 - num - num2)
+console.log("Multiplication Operator", num * num2)
+console.log("Division Operator", num / num2)
+console.log("Modulus Operator", num % num2)
+
+
+var a="Have a good Day!"
+{
+    console.log(a)
+
+}
+
